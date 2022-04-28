18097741584e6e789ba33ed8a8abb7981d9b31fc
houyueke <442067356@qq.com>
2022-04-28 11:41:22 +0800

    feat: 修改主题配置方案

--- src/config/index.js
+++ src/config/index.js
@@ -1,26 +1,28 @@
 /*
- * @Description: 全局配置项
+ * @Description: antd vue 主题配置项
  * @Autor: houyueke
  * @Date: 2022-04-22 10:38:18
  * @LastEditors: houyueke
- * @LastEditTime: 2022-04-22 14:07:25
+ * @LastEditTime: 2022-04-28 11:38:20
  */
-module.exports = {
-    themeColor: '#1ebda0', //主题色
-    linkColor: '#1890ff', // 链接色
-    successColor: '#52c41a', // 成功色
-    warningColor: '#faad14', // 警告色
-    errorColor: '#f5222d', // 错误色
-    fontSizeBase: '14px', // 主字号
-    headingColor: 'rgba(0, 0, 0, 0.85)', // 标题色
-    textColor: 'rgba(0, 0, 0, 0.65)', // 主文本色
-    textColorSecondary: 'rgba(0, 0, 0, 0.45)', // 次文本色
-    disabledColor: 'rgba(0, 0, 0, 0.25)', // 失效色
-    borderRadiusBase: '4px', // 组件/浮层圆角
-    borderColorBase: '#d9d9d9', // 边框色
-    boxShadowBase: '0 2px 8px rgba(0, 0, 0, 0.15)', // 浮层阴影
-    layout: 'side', //导航布局
-    fixedHeader: true, //固定头部
-    fixedSideBar: true //固定侧边栏
+function modifyVars() {
+  return {
+    'primary-color': '#1ebda0', // 全局主色
+    'link-color': '#1890ff', // 链接色
+    'success-color': '#52c41a', // 成功色
+    'warning-color': '#faad14', // 警告色
+    'error-color': '#f5222d', // 错误色
+    'font-size-base': '14px', // 主字号
+    'heading-color': 'rgba(0, 0, 0, 0.85)', // 标题色
+    'text-color': 'rgba(0, 0, 0, 0.65)', // 主文本色
+    'text-color-secondary': 'rgba(0, 0, 0, 0.45)', // 次文本色
+    'disabled-color': 'rgba(0, 0, 0, 0.25)', // 失效色
+    'border-radius-base': '4px', // 组件/浮层圆角
+    'border-color-base': '#d9d9d9', // 边框色
+    'box-shadow-base': '0 2px 8px rgba(0, 0, 0, 0.15)' // 浮层阴影
   }
-  
+}
+
+module.exports = {
+  modifyVars
+}
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@
  * @Autor: houyueke
  * @Date: 2022-04-12 11:54:11
  * @LastEditors: houyueke
- * @LastEditTime: 2022-04-28 11:07:16
+ * @LastEditTime: 2022-04-28 11:38:32
  */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -15,41 +15,9 @@ import {
   AndDesignVueResolve
 } from 'vite-plugin-style-import'
 
+const { modifyVars } = require('./src/config/index')
+
 const path = require('path')
-
-const {
-  themeColor,
-  linkColor,
-  successColor,
-  warningColor,
-  errorColor,
-  fontSizeBase,
-  headingColor,
-  textColor,
-  textColorSecondary,
-  disabledColor,
-  borderRadiusBase,
-  borderColorBase,
-  boxShadowBase
-} = require('./src/config/index')
-
-function modifyVars() {
-  return {
-    'primary-color': themeColor, // 全局主色
-    'link-color': linkColor, // 链接色
-    'success-color': successColor, // 成功色
-    'warning-color': warningColor, // 警告色
-    'error-color': errorColor, // 错误色
-    'font-size-base': fontSizeBase, // 主字号
-    'heading-color': headingColor, // 标题色
-    'text-color': textColor, // 主文本色
-    'text-color-secondary': textColorSecondary, // 次文本色
-    'disabled-color': disabledColor, // 失效色
-    'border-radius-base': borderRadiusBase, // 组件/浮层圆角
-    'border-color-base': borderColorBase, // 边框色
-    'box-shadow-base': boxShadowBase // 浮层阴影
-  }
-}
 
 export default defineConfig({
   resolve: {
