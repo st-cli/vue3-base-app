947ebda38eb037a8beac58682e220d01df71f9e9
yang1234567 <1015907316@qq.com>
2022-05-23 19:49:27 +0800

    feat: layout布局组件的封装(左右结构)

---
+++ src/components/UserInfo/src/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="UserInfo">
+    <bell-outlined />
+    <a-avatar v-if="userImg" :src="userImg" :size="24" />
+    <a-avatar v-else style="background-color: #357fdc" :size="24">{{
+      userName.substring(0, 1)
+    }}</a-avatar>
+    <span>欢迎您，{{ userName }}</span>
+  </div>
+</template>
+
+<script setup>
+import { BellOutlined } from '@ant-design/icons-vue'
+import { useUserStore } from '@/store/useUserStore'
+const userStore = useUserStore()
+const userName = userStore.getUserName
+const userImg = userStore.getUserImg || ''
+</script>
+
+<style lang="less" scoped>
+.UserInfo {
+  float: right;
+  padding-right: 16px;
+}
+.ant-avatar {
+  margin: -3px 8px 0 24px;
+}
+</style>
